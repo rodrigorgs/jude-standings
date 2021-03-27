84e35108af173b70f935cfe3d1269a08d5403ec4
Rodrigo Rocha <rodrigorgs@gmail.com>
2021-03-26 23:14:15 -0300

    Implementa corretamente com o BeautifulSoup

--- extract-standings.py
+++ extract-standings.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from bs4 import BeautifulSoup
 from selenium import webdriver  
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.chrome.options import Options  
@@ -9,59 +10,62 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 import time 
 
+driver = None
 
-chrome_options = Options()  
-# chrome_options.add_argument('--headless')  
-chrome_options.add_argument('--window-size=1280,800')
+def init_webdriver():
+	if driver is None:
+		chrome_options = Options()  
+		# chrome_options.add_argument('--headless')  
+		chrome_options.add_argument('--window-size=1280,800')
+		driver = webdriver.Chrome(options = chrome_options)
 
-driver = webdriver.Chrome(options = chrome_options)
-driver.implicitly_wait(30)
+# 'http://200.128.51.30/#/login?id=603bcc937293660011629359'
+def get_standings_html(driver, url):
+	driver.implicitly_wait(30)
 
-driver.get('http://200.128.51.30/#/login?id=603bcc937293660011629359')
+	driver.get(url)
 
-inputs = driver.find_elements_by_css_selector('input')
-inputs[0].send_keys(os.getenv('JUDE_USER'))
-inputs[1].send_keys(os.getenv('JUDE_PASSWORD'))
+	inputs = driver.find_elements_by_css_selector('input')
+	inputs[0].send_keys(os.getenv('JUDE_USER'))
+	inputs[1].send_keys(os.getenv('JUDE_PASSWORD'))
 
-driver.find_element_by_link_text('Login').click()
-time.sleep(2)
-driver.get('http://200.128.51.30/#/contest/standings')
+	driver.find_element_by_link_text('Login').click()
+	time.sleep(2)
+	driver.get('http://200.128.51.30/#/contest/standings')
 
-elems = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#floatTheadTable tbody tr")))
+	elems = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#floatTheadTable tbody tr")))
 
-for elem in elems:
-	children = elem.find_elements_by_css_selector('*')
-	nome = children[2].get_attribute('innerText')
-	score = children[3].get_attribute('innerText').split('\n')[0]
-	details = []
-	for question_elem in children[4:]:
-		driver.implicitly_wait(0)
-		p_elems = question_elem.find_elements_by_tag_name('p')
-		
-		correct = 0
-		if len(p_elems) > 0:
-			pts = p_elems[0].get_attribute('innerText').strip()
-			if '+' in pts:
-				correct = 1
-		details.append(correct)
+	return driver.page_source
 
-	print(f'nome: {nome}\tscore: {score}\tdetails: {" ".join([str(x) for x in details])}')
+def extract_data(html):
+	data = []
+	soup = BeautifulSoup(html, 'html.parser')
+	for elem in soup.select('#floatTheadTable tbody tr'):
+		children = elem.select('td')
+		nome = children[1].text.strip()
+		score = children[2].select('p')[0].text.strip()
+		details = []
+		for question in children[3:]:
+			correct = '0'
+			p = question.select('p')
+			if len(p) > 0 and '+' in p[0].text:
+				correct = '1'
+			details.append(correct)
+		assert sum([int(x) for x in details]) == int(score)
+		data.append([nome, score, *details])
 
-# x = driver.page_source
-# print(x)
+	return sorted(data, key=lambda x: x[0])
+
+def save_data_as_csv(data, filename):
+	with open(filename, 'w') as f:
+		for aluno in data:
+			f.write("\t".join(aluno) + "\n")
 
 
-#####
-
-# var x = Array.from(document.querySelectorAll('#floatTheadTable tbody tr'))
-#   .map(e => ({
-#     nome: e.children[1].innerText,
-#     score: e.children[2].innerText.split('\n')[0],
-#     detail: Array.from(e.children).slice(3).map(elem => elem.children.length == 0 ? '' : elem.children[0].children[0].innerText.trim().replace(/[+]$/, '0')),
-#     time: Array.from(e.querySelectorAll('.ju-problem-cell:not(.ju-score-cell) .ju-score-info')).reduce((last, elem) => Math.max(last, parseInt(elem.innerText.split(':')[0])), 0)
-# }));
-# x.forEach(e => {
-#   e.correct = e.detail.map(d => (parseInt(d) >= 0 ? 1 : 0))}
-# );
-# var y = x.map(e => `${e.nome}\t${e.score}\t${e.correct.join('\t')}`).sort().join('\n')
-# console.log(y);
+if __name__ == "__main__":
+	with open('exemplo.html', 'r') as f:
+		html = f.read()
+		data = extract_data(html)
+		# save_data_as_csv(data, 'teste.csv')
+		for aluno in data:
+		print("\t".join(aluno))
